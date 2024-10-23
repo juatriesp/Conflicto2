591729b8daf64fa8ea34537685193be5b6fc9a6d
raulmarinmorales308 <rmarmor308@iesalmudeyne.es>
2024-10-23 09:44:25 +0200

    Primera subida de raul modificada

--- suma.java
+++ suma.java
@@ -1,10 +1,12 @@
 public class Suma {
     public static void main(String[] args) {
-        int numero1 = 10;
-        int numero2 = 20;
+        int numero1 = 10000;
+        int numero2 = 20000;
 
         int resultado = numero1 + numero2;
 
         System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
+    
+        
     }
 }
