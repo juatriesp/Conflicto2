0048015ba53de137b9608ce37e95ad90b4c4ca3a
juatriesp <jtriesp073@iesalmudeyne.es>
2024-10-23 09:33:27 +0200

    Primer commit - Suma

---
+++ suma.java
@@ -0,0 +1,10 @@
+public class Suma {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+
+        int resultado = numero1 + numero2;
+
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
+    }
+}
